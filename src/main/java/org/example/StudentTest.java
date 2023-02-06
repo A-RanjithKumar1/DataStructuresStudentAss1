57dbd8006f567d8296cdfff6a5036165b81f422f
Tringapps-Ranjithkumar <ananthula.kumar@tringapps.com>
2023-02-06 17:46:31 +0530

    seventh commit

--- src/main/java/org/example/StudentTest.java
+++ src/main/java/org/example/StudentTest.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.logging.*;
 import java.util.*;
 class Student {
-    private static final Logger Logger = java.util.logging.Logger.getLogger("InfoLogging");
+    private static final Logger LOGger = Logger.getLogger("InfoLogging");
     private String name;
     private int age;
     private float gpa;
@@ -41,7 +41,7 @@ class Student {
 
     public static void printDetails(List<Student> al) {
         for (Student st : al) {
-            Logger.info(st.getName() + ", " + st.getAge() + ", " + st.getGpa());
+            LOGger.info(st.getName() + ", " + st.getAge() + ", " + st.getGpa());
         }
     }
 }
@@ -62,32 +62,32 @@ class Student {
     }
     public class StudentTest {
         public static void main(String[] args) {
-            final Logger Logger = java.util.logging.Logger.getLogger("InfoLogging");
+            final Logger LOG = Logger.getLogger("InfoLogging");
             Scanner sc = new Scanner(System.in);
             ArrayList<Student> al = new ArrayList<Student>();
-            Logger.info("Enter the no of students details to be registered");
+            LOG.info("Enter the no of students details to be registered");
             int n = sc.nextInt();
             while (n > 0) {
-                Logger.info("Please Enter the Student Name");
+                LOG.info("Please Enter the Student Name");
                 String name = sc.next();
-                Logger.info("Enter the Age");
+                LOG.info("Enter the Age");
                 int age = sc.nextInt();
-                Logger.info("Enter the Gpa");
+                LOG.info("Enter the Gpa");
                 float gpa = sc.nextFloat();
                 al.add(new Student(name, age, gpa));
                 n -= 1;
-            };
+            }
             int op =1;
             while (op>0 && op<=2) {
-                Logger.info("Select the below Operations" + "\n" + "1.PrintDetails of each Student" + "\n" + "2.Sort the list by highest to lowest with respect to GPA and Display" + "\n" + "3.Exit");
+                LOG.info("Select the below Operations" + "\n" + "1.PrintDetails of each Student" + "\n" + "2.Sort the list by highest to lowest with respect to GPA and Display" + "\n" + "3.Exit");
                 op = sc.nextInt();
                 if (op == 1) {
                     Student.printDetails(al);
                 } else if (op == 2) {
                     Collections.sort(al, new SortbyGpa());
-                    Logger.info("After Sorting");
+                    LOG.info("After Sorting");
                     for (Student st : al) {
-                        Logger.info(st.getName() + ", " + st.getAge() + ", " + st.getGpa());
+                        LOG.info(st.getName() + ", " + st.getAge() + ", " + st.getGpa());
                     }
                     op=10;
                 }
